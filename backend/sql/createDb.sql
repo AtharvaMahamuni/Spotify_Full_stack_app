0e231eb73ebba44e11fe6f2a173ecd300a1ac7d5
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2022-06-02 21:25:19 +0530

    sql

--- backend/sql/createDb.sql
+++ backend/sql/createDb.sql
@@ -42,7 +42,6 @@ DESC artist_song;
 CREATE TABLE user (
   user_id INT NOT NULL AUTO_INCREMENT,
   user_name VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
   PRIMARY KEY (user_id)
 );
 DESC user;
